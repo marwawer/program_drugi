0bbaa3e4da4e844d60bba1bcc5a0fe1d92dbeb01
marwawer <marwawer@hotmail.com>
2017-10-19 17:48:22 +0200

    dwa kolejne programy

---
+++ program2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main () 
+{ 
+
+	putchar('v');	/*wypisz znak*/
+
+	putchar('v'); 
+
+	puts("wpisz string / ciag znakow");
+
+	puts("\nAla ma kota,\na kot ma Ale");
+
+	return 0; 
+
+}
